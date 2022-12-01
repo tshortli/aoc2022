06e24e87c110856ec972b5f2ab08987445e666c9
Allan Shortlidge <tshortli@gmail.com>
2022-11-30 22:32:37 -0800

    Day 1: Solve part 1 and 2.

---
+++ Day01/Sources/Day01/Day01.swift
@@ -0,0 +1,37 @@
+public struct Day01 {
+  let input: String
+  let elfCount: Int
+
+  public init(input: String, elfCount: Int) {
+    assert(elfCount > 0)
+
+    self.input = input
+    self.elfCount = elfCount
+  }
+
+  var caloriesPerElf: [Int] {
+    var calories: [Int] = []
+    var currentTotalCalories = 0
+    for line in input.lines {
+      if line.isEmpty {
+        calories.append(currentTotalCalories)
+        currentTotalCalories = 0
+      } else {
+        currentTotalCalories += Int(line)!
+      }
+    }
+    calories.append(currentTotalCalories)
+
+    return calories
+  }
+
+  var solution: Int {
+    caloriesPerElf.sorted().suffix(elfCount).reduce(0, +)
+  }
+}
+
+private extension String {
+  var lines: [Substring] {
+    split(separator: "\n", omittingEmptySubsequences: false)
+  }
+}
