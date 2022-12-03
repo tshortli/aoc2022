070e72ed3999a4b73db7e9fb9f0d20664516e3a8
Allan Shortlidge <tshortli@gmail.com>
2022-12-02 20:59:45 -0800

    Day 3: Set up.

---
+++ Day03/Sources/Day03/Day03.swift
@@ -0,0 +1,7 @@
+public struct Day03 {
+  let input: String
+
+  public init(input: String) {
+    self.input = input
+  }
+}
---
+++ Day03/Tests/Day03Tests/Day03Tests.swift
@@ -0,0 +1,18 @@
+import XCTest
+@testable import Day03
+
+final class Day03Tests: XCTestCase {
+  let exampleInput = """
+
+    """
+  
+  func testPart1Example() throws {
+    let solver = Day03(input: exampleInput)
+
+  }
+
+  func testPart1() throws {
+    let solver = Day03(input: puzzleInput)
+
+  }
+}
