2ee4264ef18f96f02a84213f8f5910a4621e01a4
Allan Shortlidge <tshortli@gmail.com>
2022-12-02 21:15:34 -0800

    Day 3: Solve part 1.

--- Day03/Sources/Day03/Day03.swift
+++ Day03/Sources/Day03/Day03.swift
@@ -4,4 +4,33 @@ public struct Day03 {
   public init(input: String) {
     self.input = input
   }
+
+  var itemTypeSum: Int {
+    input.lines
+      .map { duplicateItem(in: $0) }
+      .map { priority($0) }
+      .reduce(0, +)
+  }
+
+  func duplicateItem(in line: Substring) -> Character {
+    let compartment1 = Set(line.prefix(line.count / 2))
+    let compartment2 = Set(line.suffix(line.count / 2))
+    return compartment1.intersection(compartment2).first!
+  }
+
+  func priority(_ c: Character) -> Int {
+    if c.asciiValue! > Character("a").asciiValue! {
+      return Int(c.asciiValue! - Character("a").asciiValue!) + 1
+    }
+    if c.asciiValue! > Character("A").asciiValue! {
+      return Int(c.asciiValue! - Character("A").asciiValue!) + 27
+    }
+    fatalError()
+  }
+}
+
+private extension String {
+  var lines: [Substring] {
+    split(separator: "\n")
+  }
 }
--- Day03/Tests/Day03Tests/Day03Tests.swift
+++ Day03/Tests/Day03Tests/Day03Tests.swift
@@ -3,16 +3,21 @@ import XCTest
 
 final class Day03Tests: XCTestCase {
   let exampleInput = """
-
+    vJrwpWtwJgWrhcsFMMfFFhFp
+    jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+    PmmdzqPrVvPwwTWBwg
+    wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+    ttgJtRGJQctTZtZT
+    CrZsJsPPZsGzwwsLwLmpwMDw
     """
   
   func testPart1Example() throws {
     let solver = Day03(input: exampleInput)
-
+    XCTAssertEqual(solver.itemTypeSum, 157)
   }
 
   func testPart1() throws {
     let solver = Day03(input: puzzleInput)
-
+    XCTAssertEqual(solver.itemTypeSum, 7811)
   }
 }
