9632bd4e21339c35ae5152fe6484be1f632013fb
Allan Shortlidge <tshortli@gmail.com>
2022-12-06 21:59:58 -0800

    Day 7: Solve part 1.

--- Sources/aoc/Day07.swift
+++ Sources/aoc/Day07.swift
@@ -5,7 +5,51 @@ public struct Day07 {
     self.input = input
   }
 
+
   public var part1Solution: Int {
-    0
+    var cwd: [String] = []
+    var sizeStack: [Int] = []
+    var sizeByDirectory: [String: Int] = [:]
+
+    for line in input.lines {
+      if line.hasPrefix("$ cd ") {
+        let directoryName = line.suffix(line.count - 5)
+        if directoryName == ".." {
+          let cwdSize = sizeStack.last!
+          sizeByDirectory[cwd.joined(separator: "/")] = cwdSize
+          cwd.removeLast()
+          sizeStack.removeLast()
+          sizeStack[sizeStack.count - 1] += cwdSize
+        } else {
+          cwd.append(String(directoryName == "/" ? "" : directoryName))
+          sizeStack.append(0)
+        }
+      } else if line.hasPrefix("$ ls") {
+        // Nothing?
+      } else if line.hasPrefix("dir ") {
+        // Nothing?
+      } else {
+        let components = line.split(separator: " ")
+        sizeStack[sizeStack.count - 1] += Int(String(components[0]))!
+      }
+    }
+
+    // Finish unwinding the stacks
+    while cwd.count > 1 {
+      let cwdSize = sizeStack.last!
+      sizeByDirectory[cwd.joined(separator: "/")] = cwdSize
+      cwd.removeLast()
+      sizeStack.removeLast()
+      sizeStack[sizeStack.count - 1] += cwdSize
+    }
+
+    return sizeByDirectory
+      .map(\.value)
+      .filter { $0 <= 100000 }
+      .reduce(0, +)
   }
+}
+
+struct SizeEnumerator {
+
 }
