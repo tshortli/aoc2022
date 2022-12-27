e4b9f99082bbc09b55713f4759d5b7bed5fd6378
Allan Shortlidge <tshortli@gmail.com>
2022-12-26 19:41:04 -0500

    Day 16: Optimize by avoiding allocations and hashing (part 2 ~19s -> ~4s).

--- Sources/aoc/Day16.swift
+++ Sources/aoc/Day16.swift
@@ -8,41 +8,54 @@ public struct Day16: Solver {
   }
 
   public var part1Solution: Int {
-    let map = ValveMap(valvesByID: valvesByID)
-    let remainingIDs = map.valvesByID.values
-      .filter { $0.flowRate > 0 }
-      .map(\.id)
+    let map = ValveMap(valves: parseValves())
+    var availableValves = Bitset(count: map.valves.count, initialValue: true)
+    for (index, valve) in map.valves.enumerated() where valve.flowRate == 0 {
+      availableValves[index] = false
+    }
 
     return map.bestFlow(
-      currentID: Valve.ID(from: "AA"),
+      currentIndex: map.startIndex,
       remainingMin: 30,
-      remainingIDs: remainingIDs
+      availableValves: &availableValves
     )
   }
 
   public var part2Solution: Int {
-    let map = ValveMap(valvesByID: valvesByID)
-    let allRemainingIDs = map.valvesByID.values
-      .filter { $0.flowRate > 0 }
-      .map(\.id)
+    let map = ValveMap(valves: parseValves())
+    let availableIndices = map.valves.reduce(into: Set<Int>()) { indices, valve in
+      if valve.flowRate > 0 {
+        indices.insert(valve.index)
+      }
+    }
 
-    let startID = Valve.ID(from: "AA")
+    var availableValves = Bitset(count: map.valves.count)
+    for index in availableIndices {
+      availableValves[index] = true
+    }
+
     var maxFlow = Int.min
-    for count in 1...(allRemainingIDs.count / 2) {
-      for elephantIDs in allRemainingIDs.combinations(ofCount: count) {
-        let usedIDsSet = Set(elephantIDs)
-        let humanIDs = allRemainingIDs.filter { !usedIDsSet.contains($0) }
-
+    for count in 1...(availableIndices.count / 2) {
+      for elephantIndices in availableIndices.combinations(ofCount: count) {
+        let elephantIndices = Set(elephantIndices)
+        var elephantValves = availableValves
+        for index in availableIndices where !elephantIndices.contains(index) {
+          elephantValves[index] = false
+        }
         let elephantFlow = map.bestFlow(
-          currentID: startID,
+          currentIndex: map.startIndex,
           remainingMin: 26,
-          remainingIDs: elephantIDs
+          availableValves: &elephantValves
         )
 
+        var humanValves = availableValves
+        for index in availableIndices where elephantIndices.contains(index) {
+          humanValves[index] = false
+        }
         let humanFlow = map.bestFlow(
-          currentID: startID,
+          currentIndex: map.startIndex,
           remainingMin: 26,
-          remainingIDs: humanIDs
+          availableValves: &humanValves
         )
 
         maxFlow = max(maxFlow, elephantFlow + humanFlow)
@@ -52,74 +65,88 @@ public struct Day16: Solver {
     return maxFlow
   }
 
-  var valvesByID: [Valve.ID: Valve] {
-    input.lines
-      .map { Valve(from: $0) }
-      .reduce(into: [:]) { dict, valve in
-        dict[valve.id] = valve
-      }
+  func parseValves() -> [Valve] {
+    var index = 0
+    var valves: [Valve] = []
+    var valveIndicesByName: [String: Int] = [:]
+    for line in input.lines {
+      let valve = Valve(from: line, index: index)
+      valves.append(valve)
+      valveIndicesByName[valve.name] = index
+      index += 1
+    }
+    return valves
   }
 
   struct ValveMap {
-    let valvesByID: [Valve.ID: Valve]
-    let distancesByID: [Valve.ID: [Valve.ID: Int]]
+    let valves: [Valve]
+    let startIndex: Int
+    let distancesByIndex: [[Int]]
 
-    init(valvesByID: [Valve.ID: Valve]) {
-      self.valvesByID = valvesByID
-      self.distancesByID = Self.distancesByValveID(valves: valvesByID)
+    init(valves: [Valve]) {
+      self.valves = valves
+      self.startIndex = valves.filter { $0.name == "AA" }.first!.index
+      self.distancesByIndex = Self.distancesByIndex(valves: valves)
     }
 
-    static func distancesByValveID(
-      valves: [Valve.ID: Valve]
-    ) -> [Valve.ID: [Valve.ID: Int]] {
-      var distancesByValveName: [Valve.ID: [Valve.ID: Int]] = [:]
-      for fromValve in valves.values {
-        var distances: [Valve.ID: Int] = [:]
-        var queue: [(Valve.ID, Int)] = fromValve.tunnels.map { ($0, 1) }
+    static func distancesByIndex(
+      valves: [Valve]
+    ) -> [[Int]] {
+      let valvesByName = valves.reduce(into: [:]) { dict, valve in
+        dict[valve.name] = valve
+      }
+      var distancesByIndex: [[Int]] = []
+      for fromValve in valves {
+        var distances: [Int] = Array(repeating: Int.max, count: valves.count)
+        var queue: [(String, Int)] = fromValve.tunnelNames.map { ($0, 1) }
         var queueIndex = 0
 
         while queueIndex < queue.count {
-          let (toValveID, distance) = queue[queueIndex]
-          distances[toValveID] = distance
-          let toValve = valves[toValveID]!
-          for tunnel in toValve.tunnels {
-            if distances[tunnel] == nil && tunnel != fromValve.id {
-              queue.append((tunnel, distance + 1))
+          let (toValveName, distance) = queue[queueIndex]
+          let toValve = valvesByName[toValveName]!
+          distances[toValve.index] = distance
+          for tunnelName in toValve.tunnelNames {
+            let tunnelIndex = valvesByName[tunnelName]!.index
+            if distances[tunnelIndex] == Int.max && tunnelIndex != fromValve.index {
+              queue.append((tunnelName, distance + 1))
             }
           }
           queueIndex += 1
         }
 
-        distancesByValveName[fromValve.id] = distances
+        assert(distancesByIndex.count == fromValve.index)
+        distancesByIndex.append(distances)
       }
-      return distancesByValveName
+      return distancesByIndex
     }
 
     func bestFlow(
-      currentID: Valve.ID,
+      currentIndex: Int,
       remainingMin: Int,
-      remainingIDs: some Sequence<Valve.ID>,
+      availableValves: inout Bitset,
       flowPerMin: Int = 0,
       totalFlow: Int = 0
     ) -> Int {
-      let distances = distancesByID[currentID]!
+      let distances = distancesByIndex[currentIndex]
       var maxFlow = Int.min
-      for id in remainingIDs {
-        let minToOpen = distances[id]! + 1
+      for index in availableValves.indices(where: true) {
+        let minToOpen = distances[index] + 1
         let newRemainingMin = remainingMin - minToOpen
         if newRemainingMin < 0 { continue }
 
-        let newFlowPerMin = flowPerMin + valvesByID[id]!.flowRate
+        let newFlowPerMin = flowPerMin + valves[index].flowRate
         let newTotalFlow = totalFlow + flowPerMin * minToOpen
-        let newRemainingIDs = remainingIDs.filter { $0 != id }
+        availableValves[index] = false
 
         maxFlow = max(maxFlow, bestFlow(
-          currentID: id,
+          currentIndex: index,
           remainingMin: newRemainingMin,
-          remainingIDs: newRemainingIDs,
+          availableValves: &availableValves,
           flowPerMin: newFlowPerMin,
           totalFlow: newTotalFlow
         ))
+
+        availableValves[index] = true
       }
 
       if maxFlow > Int.min {
@@ -130,29 +157,16 @@ public struct Day16: Solver {
   }
 
   struct Valve {
-    struct ID: Equatable, Hashable {
-      let value: Int
-
-      init(from string: some StringProtocol) {
-        var radix = 1, value = 0
-        for c in string.utf8.reversed() {
-          value += Int(c) * radix
-          radix *= Int(UInt8.max)
-        }
-        self.value = value
-      }
-    }
-
-    let id: ID
+    let index: Int
     let name: String
     let flowRate: Int
-    let tunnels: [ID]
+    let tunnelNames: [String]
 
-    init(from string: some StringProtocol) {
+    init(from string: some StringProtocol, index: Int) {
+      self.index = index
       var parser = Parser(string)
       parser.consume("Valve ")
       self.name = String(parser.advance(2))
-      self.id = ID(from: name)
       parser.consume(" has flow rate=")
       self.flowRate = parser.parseInt()
       parser.consume("; tunnel")
@@ -162,13 +176,68 @@ public struct Day16: Solver {
         parser.consume(" leads to valve")
       }
       parser.consume(parser.peek() == "s" ? "s " : " ") // Ugh why
-      var tunnels: [Day16.Valve.ID] = []
-      tunnels.append(Day16.Valve.ID(from: parser.advance(2)))
+      var tunnelNames: [String] = []
+      tunnelNames.append(String(parser.advance(2)))
       while parser.hasMore {
         parser.consume(", ")
-        tunnels.append(Day16.Valve.ID(from: parser.advance(2)))
+        tunnelNames.append(String(parser.advance(2)))
       }
-      self.tunnels = tunnels
+      self.tunnelNames = tunnelNames
+    }
+  }
+
+  struct Bitset {
+    var values: UInt64
+    let count: Int
+
+    init(count: Int, initialValue: Bool = false) {
+      precondition(count <= UInt64.bitWidth)
+      self.values = initialValue ? ~0 : 0
+      self.count = count
+    }
+
+    subscript(_ index: Int) -> Bool {
+      get {
+        assert(index < count)
+        return (values & (1 << index)) != 0
+      }
+      set {
+        assert(index < count)
+        if newValue {
+          values |= 1 << index
+        } else {
+          values &= ~(1 << index)
+        }
+      }
+    }
+
+    func indices(where predicate: Bool) -> IndexSequence {
+      IndexSequence(base: self, predicate: predicate)
+    }
+
+    struct IndexSequence: Swift.Sequence {
+      let base: Bitset
+      let predicate: Bool
+
+      struct Iterator: IteratorProtocol {
+        let base: Bitset
+        let predicate: Bool
+        var index: Int = 0
+
+        mutating func next() -> Int? {
+          for i in index..<base.count {
+            if base[i] == predicate {
+              index = i + 1
+              return i
+            }
+          }
+          return nil
+        }
+      }
+
+      func makeIterator() -> Iterator {
+        Iterator(base: base, predicate: predicate)
+      }
     }
   }
 }
